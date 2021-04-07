32f414c383318bbe3cace470fe2209d35a9ec924
Jonathan Ehwald <github@ehwald.info>
2021-04-07 22:23:01 +0200

    Add tests

---
+++ tests/settings.py
@@ -0,0 +1,44 @@
+DEBUG = True
+
+SECRET_KEY = "fake-key"
+
+ROOT_URLCONF = "tests.urls"
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "db.sqlite"}}
+
+INSTALLED_APPS = [
+    "admin_notice",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.staticfiles",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "admin_notice.context_processors.notice",
+            ]
+        },
+    },
+]
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+STATIC_URL = "static/"
+
+ADMIN_NOTICE_TEXT = "Test environment"
+ADMIN_NOTICE_TEXT_COLOR = "white"
+ADMIN_NOTICE_BACKGROUND = "red"
---
+++ tests/tests/test_rendering.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from faker import Faker
+
+
+fake = Faker()
+RENDER_INDICATOR = "<!-- django-admin-notice -->"
+
+
+class RenderingTestCase(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        superuser = get_user_model().objects.create_superuser(
+            username="admin", email=fake.email(), password=fake.password()
+        )
+        self.client.force_login(superuser)
+
+    def test_does_not_render_without_required_settings(self):
+        with self.settings():
+            del settings.ADMIN_NOTICE_TEXT
+            response = self.client.get("/admin/")
+            self.assertNotContains(response, RENDER_INDICATOR)
+
+    def test_renders_configured_text(self):
+        text = fake.sentence()
+        with self.settings(ADMIN_NOTICE_TEXT=text):
+            response = self.client.get("/admin/")
+            self.assertContains(response, RENDER_INDICATOR)
+            self.assertContains(response, text)
+
+    def test_renders_configured_text_color(self):
+        color = fake.rgb_css_color()
+        with self.settings(ADMIN_NOTICE_TEXT="Test", ADMIN_NOTICE_TEXT_COLOR=color):
+            response = self.client.get("/admin/")
+            self.assertContains(response, RENDER_INDICATOR)
+            self.assertContains(response, f"color: {color}")
+
+    def test_renders_configured_background(self):
+        color = fake.rgb_css_color()
+        with self.settings(ADMIN_NOTICE_TEXT="Test", ADMIN_NOTICE_BACKGROUND=color):
+            response = self.client.get("/admin/")
+            self.assertContains(response, RENDER_INDICATOR)
+            self.assertContains(response, f"background: {color}")
+
+    def test_renders_fallback_if_no_text_color_is_configured(self):
+        with self.settings():
+            del settings.ADMIN_NOTICE_TEXT_COLOR
+            response = self.client.get("/admin/")
+            self.assertContains(response, RENDER_INDICATOR)
+            self.assertContains(response, "color: white")
+
+    def test_renders_fallback_if_not_background_is_configured(self):
+        with self.settings():
+            del settings.ADMIN_NOTICE_BACKGROUND
+            response = self.client.get("/admin/")
+            self.assertContains(response, RENDER_INDICATOR)
+            self.assertContains(response, "background: red")
