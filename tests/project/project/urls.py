bdf082603c29488a697e9a05fced18f13c93674f
Jonathan Ehwald <github@ehwald.info>
2023-05-06 05:22:55 +0200

    Redirect to the admin page

--- tests/project/project/urls.py
+++ tests/project/project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="/admin/")),
 ]
